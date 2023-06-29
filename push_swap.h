a68e5d460fe9639e9668ea336852f31bdef7d574
jmsmg <jmsmg1@me.com>
2023-06-29 19:41:14 +0900

    list 정보를 담은 구조체

--- push_swap.h
+++ push_swap.h
@@ -7,5 +7,11 @@ typedef struct s_node
     struct s_node  *next;   
 } t_node;
 
+typedef struct s_lst
+{
+    int             len;
+    struct s_node   *head;
+} t_lst;
+
 #define INT_MAX 2147483647
 #define INT_MIN -2147483648
