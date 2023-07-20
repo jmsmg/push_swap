9ae5be17f959f214355fe3622d102bb50d516ffe
jmsmg <jmsmg1@me.com>
2023-07-20 17:03:42 +0900

    main 이름 변경

--- push_swap.c
+++ push_swap.c
@@ -1,33 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seonggoc <seonggoc@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 09:01:36 by seonggoc          #+#    #+#             */
-/*   Updated: 2023/07/18 10:47:25 by seonggoc         ###   ########.fr       */
+/*   Updated: 2023/07/20 16:34:59 by seonggoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	t_info *stack;
+	t_info	*stack;
 
 	if (argc < 2 && !ft_check_validation(argc, argv))
 	{
-		write(1, "Error\n", 6);
+		return (write(1, "Error\n", 6));
 	}
 	stack = ft_get_stack(argc, argv);
 	if (!(stack->a_head))
 	{
 		return (write(1, "Error\n", 6));
 	}
-
-	// 자료 조작
-	write(1, "\n", 1);
 }
-
