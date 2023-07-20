e66cf59b2018563d26e940eb6a897b22b0d1dd9a
jmsmg <jmsmg1@me.com>
2023-07-20 14:09:35 +0900

    swap종류 함수 완료

--- swap_cmd.c
+++ swap_cmd.c
@@ -1,9 +1,39 @@
 #include "push_swap.h"
 
-void	ft_swap_a(t_lst **stack)
+void	ft_swap_a(t_info **stack)
 {
-	t_lst	*a_stack;
+	t_node	*head;
+	t_node	*tmp;
 
-	a_stack = (*stack)->a_head;
+	head = (*stack)->a_head;
+	tmp = head->next;
 
+	tmp->next->prev = head;
+	head->next = tmp->next;
+	tmp->next = head;
+	tmp->prev = head->prev;
+	head->prev->next = tmp;
+	head->prev = tmp;
+}
+
+void	ft_swap_b(t_info **stack)
+{
+	t_node	*head;
+	t_node	*tmp;
+
+	head = (*stack)->b_head;
+	tmp = head->next;
+
+	tmp->next->prev = head;
+	head->next = tmp->next;
+	tmp->next = head;
+	tmp->prev = head->prev;
+	head->prev->next = tmp;
+	head->prev = tmp;
+}
+
+void	ft_swap_double(t_info **stack)
+{
+	ft_swap_a(stack);
+	ft_swap_b(stack);
 }
