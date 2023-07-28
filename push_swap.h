1666a4a592d8465282a502a0c367d297db8a3e9c
jmsmg <jmsmg1@me.com>
2023-07-28 13:55:16 +0900

    선언

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: seonggoc <seonggoc@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 09:01:40 by seonggoc          #+#    #+#             */
-/*   Updated: 2023/07/28 11:16:37 by seonggoc         ###   ########.fr       */
+/*   Updated: 2023/07/28 12:45:20 by seonggoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define PUSH_SWAP_H
 # include <unistd.h>
 # include <stdlib.h>
+
 # define TRUE 1
 # define FALSE 0
 
@@ -37,8 +38,27 @@ int		ft_check_validation(int argc, char **argv);
 int		ft_check_number(char *word);
 int		ft_check_dup(int argc, char **argv);
 int		ft_check_array(int *array, int size);
-int		*ft_get_sorted_number(int size, char **argv);
-int		*ft_get_array(int size, char **argv);
-void	ft_swap(int *a, int *b);
 
+int		ft_check_array(int *array, int size);
+t_node	*ft_input_arg(int argc, char **argv);
+void	ft_data_to_idx(t_node *node, int size, int *array);
+
+void	pa(t_info **stack);
+void	pb(t_info **stack);
+
+void	ra(t_info **stack);
+void	rb(t_info **stack);
+void	rr(t_info **stack);
+
+void	rra(t_info **stack);
+void	rrb(t_info **stack);
+void	rrr(t_info **stack);
+
+void	ft_sort(t_info *stack, int pivot_a, int pivot_b);
+void	a_to_b(t_info *stack, int pivot_a, int pivot_b);
+void	b_to_a(t_info *stack, int pivot_a, int pivot_b);
+
+int		ft_check_except_case(t_info *stack, int size);
+int		size_three_case(t_info *stack, int *array);
+int		size_two_case(t_info *stack, int *array);
 #endif
