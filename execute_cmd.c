ae1e11d0686d81f4d92f5fbd3a00d5d667a38f60
jmsmg <jmsmg1@me.com>
2023-08-11 13:02:00 +0900

    ra, rb -> rr // rra, rrb -> rrr // 해야함

--- execute_cmd.c
+++ execute_cmd.c
@@ -6,7 +6,7 @@
 /*   By: seonggoc <seonggoc@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 19:28:57 by seonggoc          #+#    #+#             */
-/*   Updated: 2023/08/11 12:24:46 by seonggoc         ###   ########.fr       */
+/*   Updated: 2023/08/11 12:51:44 by seonggoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,27 @@
 
 void	ft_execute(t_info *stack, int *array, int cmd)
 {
-	int	i;
-
-	i = 0;
-	while (cmd != 0)
+	while (array[cmd] != 0)
+	{
+		if (0 < array[cmd])
+		{
+			rb(stack);
+			array[cmd]--;
+		}
+		else
+		{
+			rrb(stack);
+			array[cmd]++;
+		}
+	}
+	if (cmd < 0)
+		cmd = -(cmd);
+	while (cmd)
 	{
 		ra(stack);
-		i++;
+		cmd--;
 	}
-	i = 0;
-	while (array[i] != 0)
-	{
-		if (0 < array[i])
-			rb(stack);
-		else
-			rrb(stack);
-	}
+	pa(stack);
 }
 
 int	ft_get_cmd(t_info *stack, int *array)
@@ -42,9 +47,11 @@ int	ft_get_cmd(t_info *stack, int *array)
 	while (i < stack->a_len)
 	{
 		if (stack->b_len < array[i])
+		{
+			i++;
 			continue ;
-			//
-		if (i <= stack->a_len / 2)
+		}
+		else if (i < stack->a_len / 2)
 		{
 			if (tmp < i + array[i])
 				tmp = i;
