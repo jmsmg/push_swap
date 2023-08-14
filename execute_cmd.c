0e03296088c56834ef1c8e901d277219c1d2333c
jmsmg <jmsmg1@me.com>
2023-08-14 12:24:48 +0900

    그리디 완료 norm 안함

--- execute_cmd.c
+++ execute_cmd.c
@@ -6,7 +6,7 @@
 /*   By: seonggoc <seonggoc@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 19:28:57 by seonggoc          #+#    #+#             */
-/*   Updated: 2023/08/11 12:51:44 by seonggoc         ###   ########.fr       */
+/*   Updated: 2023/08/14 12:08:22 by seonggoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,48 @@
 
 void	ft_execute(t_info *stack, int *array, int cmd)
 {
-	while (array[cmd] != 0)
+	int	tmp;
+
+	tmp = cmd;
+	while (cmd && cmd < stack->a_len / 2 && 0 < array[tmp])
 	{
-		if (0 < array[cmd])
-		{
-			rb(stack);
-			array[cmd]--;
-		}
-		else
+		rr(stack);
+		array[tmp]--;
+		cmd--;
+	}
+	while (array[tmp] < 0 && stack->a_len / 2 < cmd)
+	{
+		rrr(stack);
+		array[tmp]++;
+		cmd++;
+	}
+	while (array[tmp] != 0)
+	{
+		if (array[tmp] < 0)
 		{
 			rrb(stack);
-			array[cmd]++;
+			array[tmp]++;
+		}
+		else if (0 < array[tmp])
+		{
+			rb(stack);
+			array[tmp]--;
 		}
 	}
-	if (cmd < 0)
-		cmd = -(cmd);
-	while (cmd)
+	while (0 < cmd && cmd < stack->a_len)
 	{
-		ra(stack);
-		cmd--;
+		if (cmd <= stack->a_len / 2)
+		{
+			ra(stack);
+			cmd--;
+		}
+		else if (stack->a_len / 2 < cmd)
+		{
+			rra(stack);
+			cmd++;
+		}
+		else
+			break ;
 	}
 	pa(stack);
 }
@@ -40,10 +63,12 @@ void	ft_execute(t_info *stack, int *array, int cmd)
 int	ft_get_cmd(t_info *stack, int *array)
 {
 	int	i;
+	int	cmd;
 	int	tmp;
 
 	i = 0;
-	tmp = 0;
+	cmd = 0;
+	tmp = 2147483647;
 	while (i < stack->a_len)
 	{
 		if (stack->b_len < array[i])
@@ -51,17 +76,23 @@ int	ft_get_cmd(t_info *stack, int *array)
 			i++;
 			continue ;
 		}
-		else if (i < stack->a_len / 2)
+		else if (i <= stack->a_len / 2)
 		{
-			if (tmp < i + array[i])
-				tmp = i;
+			if (i + array[i] < tmp)
+			{
+				tmp = i + array[i];
+				cmd = i;
+			}
 		}
-		else
+		else if (stack->a_len / 2 < i)
 		{
-			if (tmp < stack->a_len - i + array[i])
-				tmp = i;
+			if (stack->a_len - i + array[i] < tmp)
+			{
+				tmp = stack->a_len - i + array[i];
+				cmd = i;
+			}
 		}
 		i++;
 	}
-	return (tmp);
+	return (cmd);
 }
